5be7ada85f8dcbe2de87232cd14955a13029a320
Jon Wiswall <jonwis@microsoft.com>
2022-10-12 09:04:37 -0700

    Always compile for size, improve disasm

--- LottieGen/disasm.cmd
+++ LottieGen/disasm.cmd
@@ -1,2 +1,2 @@
-link -dump -linenumbers -disasm -out:testexe.txt x64\Release\LottieCppUwpTest\LottieCppUwpTest.exe
+link -dump -linenumbers -disasm -symbols -out:testexe.txt x64\Release\LottieCppUwpTest\LottieCppUwpTest.exe
 undname testexe.txt > testexe-readable.txt
