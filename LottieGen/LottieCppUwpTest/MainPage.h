ca90cb0f78c9881936aa982282ef4600b46cf885
Jevan Saks <jevansa@microsoft.com>
2022-10-11 19:03:57 -0700

    Add UWP Test app

---
+++ LottieGen/LottieCppUwpTest/MainPage.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+#include "AnimatedVisuals.LottieLogo1.h"
+
+namespace winrt::LottieCppUwpTest::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage()
+        {
+            // Xaml objects should not call InitializeComponent during construction.
+            // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+        }
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::LottieCppUwpTest::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
