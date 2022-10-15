4360a429cb695eafd97a21a553e5ee3768454883
Jon Wiswall <jonwis@microsoft.com>
2022-10-15 14:31:50 -0700

    Baseline tool

---
+++ LottieGen/make-baseline.cmd
@@ -0,0 +1,4 @@
+rmdir /s /q %temp%\lottiegen-baseline
+mkdir %temp%\lottiegen-baseline
+copy LottieCppUwpTest\AnimatedVisuals.LottieLogo1* %temp%\lottiegen-baseline
+copy x64\Release\LottieCppUwpTest\LottieCppUwpTest.* %temp%\lottiegen-baseline
