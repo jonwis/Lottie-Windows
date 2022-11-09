f177a375b9a79a0633cec4fd6d4c995f18a416df
Jon Wiswall <jonwis@microsoft.com>
2022-11-09 09:25:39 -0800

    Fix lottie fixup script

--- LottieGen/Before/fix-lotties.ps1
+++ LottieGen/Before/fix-lotties.ps1
@@ -1,7 +1,8 @@
 Write-Host "Running Fix-Lotties.ps1"
 gci *.cpp | %{ 
     $old = (get-content $_)
-    $new = $old -replace '.Properties,','.Properties(),'
+    $new = $old -replace '\.Properties\b','.Properties()'
+    $new = $new -replace '\.Properties\(\)\(\)','.Properties()'
 
     #Write-Host $_ "$old".Length "$new".Length
     if ("$old".Length -ne "$new".Length) { Write-Host "Fixing $_" ; Set-Content $_ -Value $new }
